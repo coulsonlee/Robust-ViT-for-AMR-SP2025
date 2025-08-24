645e40cc67973e2a3c8e6a51741e74b8d885a3d3
Gen Li <73267479+coulsonlee@users.noreply.github.com>
2025-08-24 19:07:28 -0400

    Update eval_data.sh

--- eval_data.sh
+++ eval_data.sh
@@ -3,5 +3,5 @@ prefix='/path/to/your/dataset/'
 
 # you can customize the tx_pwr / distance / sample_rate / and inter
 
-python eval_only.py --prefix $prefix --tx_pwr="['TP-10',]" --distance="['D5','D10','D15']" --sample_rate="['SR20',]" \
+python load_data.py --prefix $prefix --tx_pwr="['TP-10',]" --distance="['D5','D10','D15']" --sample_rate="['SR20',]" \
 --inter="['I0','I1']" 
